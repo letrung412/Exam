787ac795401965e2be1b2c19d5b08172f6a8d0c2
unknown <dohd2201@gmail.com>
2021-07-17 21:45:37 +0700

    Code update

--- main.js
+++ main.js
@@ -1,31 +1,69 @@
-const addTask = document.getElementById("add-new-task");
-
-function newTask() {
-    const new_task = document.getElementById("new-task");
+const addTask = document.getElementById('add-new-task');
+addTask.addEventListener("click", function () {
+    // Add-button
+    const new_task = document.getElementById('new-task');
+    console.log(new_task.value)
     const li = document.createElement("li");
-    li.innerHTML = new_task.value;
-    li.setAttribute("class", "collection-item"); //style
+    const getValue = document.createElement("h3");
+    getValue.innerHTML = new_task.value;
+    li.setAttribute("class", "item-value collection-item");
+    const ul = document.getElementsByClassName('collection');
+    ul[0].appendChild(li)
+    li.appendChild(getValue)
 
-    const ul = document.getElementsByClassName("collection");
-    ul[0].appendChild(li);
+    //Del-button
 
-    //add button
-    const button = document.createElement("button");
-    button.innerHTML = "Delete";
-    button.setAttribute("class" , "btn"); //style
-    li.appendChild(button);
+    const buttonDel = document.createElement("button");
+    buttonDel.innerText = "Delete";
+    buttonDel.setAttribute("class", "btn btn-del");
+    li.appendChild(buttonDel)
 
-    //add event
-    button.addEventListener("click" ,() => {
-        // li.innerHTML = ""
-        li.remove();
+    //Fix-button
+    const buttonFix = document.createElement("button");
+    buttonFix.innerText = "Fix";
+    buttonFix.setAttribute("class", "btn btn-fix");
+    li.appendChild(buttonFix)
+
+    //Fix-task
+    buttonFix.addEventListener("click", function () {
+        let old_value = li.innerText.slice(0, li.innerText.length - 9);
+        new_value = prompt(`Nhập giá trị muốn thay đổi cho: ${old_value}`);
+        let htmls = `${new_value}`
+        if (new_value == getValue.innerText) {
+            alert('Thông tin trùng nhau !')
+        }
+        else if (new_value === "") {
+            alert('Bạn phải nhập thông tin sửa!')
+        }
+        else {
+            getValue.innerHTML = '';
+            getValue.innerHTML = htmls
+
+        }
+
+
     })
-}
 
-addTask.addEventListener("click" , () => newTask());
+    //Del-task
 
-const deleteAll = document.getElementById("delete-all");
-deleteAll.addEventListener("click" , () => {
-    const ul = document.getElementsByClassName("collection");
-    ul[0].innerHTML = "";
+    buttonDel.addEventListener("click", function () {
+        li.remove();
+
+    })
+
 })
+
+
+//Del-all
+
+const dell_all = document.getElementById('delete-all');
+dell_all.addEventListener("click", function () {
+    const ul = document.getElementsByClassName('collection');
+    ul[0].innerHTML = '';
+})
+
+
+
+
+
+
