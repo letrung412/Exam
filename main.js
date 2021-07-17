61e7bb304a1f464c4d85ade6a842458eed5d0744
danglong119 <61708431+danglong119@users.noreply.github.com>
2021-07-18 01:45:21 +0700

    time to bed

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const ul = document.getElementsByClassName('collection')
-const addTask = document.getElementById('add-new-task')
+const btnAddTask = document.getElementById('add-new-task')
 const btnDeleteAllTasks = document.getElementById('delete-all')
 
-addTask.addEventListener('click', ()=>addNewTask())
+btnAddTask.addEventListener('click', () => addNewTask())
 
 btnDeleteAllTasks.addEventListener('click',()=>deleteAllTasks())
 
@@ -10,25 +10,51 @@ const addNewTask = () =>{
     const newTask = document.getElementById('new-task')
 
     const li = document.createElement('li')
-    li.innerHTML = newTask.value
     li.setAttribute('class', 'collection-item')
+    li.setAttribute('style', 'display: flex')
+
+    const divContent = document.createElement('div')
+    divContent.innerHTML = newTask.value
+    divContent.setAttribute('style','flex-grow: 8')
+    li.appendChild(divContent)
+
+    const btnDelete = document.createElement('button')
+    btnDelete.innerHTML = 'Delete'
+    btnDelete.setAttribute('class', 'btn')
+    btnDelete.addEventListener('click', ()=> li.remove())
+    li.appendChild(btnDelete)    
+
+    const btnUpdate = document.createElement('button')
+    btnUpdate.innerHTML = 'Update'
+    btnUpdate.setAttribute('class', 'btn')
+    btnUpdate.addEventListener('click', ()=> updateTask(li))
+    li.appendChild(btnUpdate) 
+
     ul[0].appendChild(li)
-
-    const btn = document.createElement('button')
-    btn.innerHTML = 'Delete'
-    btn.setAttribute('class', 'btn')
-    btn.addEventListener('click', ()=> li.remove())
-    li.appendChild(btn)    
-
-    const btn = document.createElement('button')
-    btn.innerHTML = 'Update'
-    btn.setAttribute('class', 'btn')
-    btn.addEventListener('click', ()=> li.remove())
-    li.appendChild(btn) 
-
     newTask.value= ''
 }
 
 const deleteAllTasks = () =>{
     ul[0].innerHTML=''
+}
+
+const updateTask = (tag )=>{
+    const arrChild = tag.children
+    for(let i=0; i< 3; i++)
+        arrChild[i].style.display = "none"
+
+    const inputUpdate = document.createElement('input')
+    inputUpdate.setAttribute('value',arrChild[0].innerText)
+    tag.appendChild(inputUpdate)
+
+    const btnConfirm = document.createElement('button')
+    btnConfirm.innerHTML = 'Confirm'
+    btnConfirm.setAttribute('class', 'btn')
+    btnConfirm.addEventListener('click', ()=> {
+        for(let i=0; i< 3; i++)
+            arrChild[i].style.display = "inline-block"
+        tag.children[3].style.display = "none"
+        tag.children[4].style.display = "none"
+    })
+    tag.appendChild(btnConfirm)
 }
