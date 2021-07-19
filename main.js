e7a9a894e757cbe2cdb617ba8c12d065f0e33f9e
nnminh31 <nhatminhnguyen3108@gmail.com>
2021-07-19 10:33:20 +0700

    Update main.js
    update them sua xoa

--- main.js
+++ main.js
@@ -1,31 +1,48 @@
-const addTask = document.getElementById("add-new-task");
+const button = document.getElementById('add-new-task');
+  
+// const dataList = [];
+button.addEventListener('click', () => {
+    const new_task = document.getElementById('new-task');
+    console.log(new_task.value);
+    const li = document.createElement('li');
+    const p = document.createElement('p');
+    p.innerHTML = new_task.value;
 
-function newTask() {
-    const new_task = document.getElementById("new-task");
-    const li = document.createElement("li");
-    li.innerHTML = new_task.value;
-    li.setAttribute("class", "collection-item"); //style
-
-    const ul = document.getElementsByClassName("collection");
+    li.setAttribute('class', 'collection-item');
+    const ul = document.getElementsByClassName('collection');
     ul[0].appendChild(li);
+    li.appendChild(p);
 
-    //add button
-    const button = document.createElement("button");
-    button.innerHTML = "Delete";
-    button.setAttribute("class" , "btn"); //style
-    li.appendChild(button);
-
-    //add event
-    button.addEventListener("click" ,() => {
-        // li.innerHTML = ""
+    //add delete button
+    const xoa = document.createElement('button');
+    xoa.innerHTML = 'Delete';
+    xoa.setAttribute('class', 'btn');
+    li.appendChild(xoa);
+    xoa.addEventListener('click', () => {
         li.remove();
     })
-}
 
-addTask.addEventListener("click" , () => newTask());
-
-const deleteAll = document.getElementById("delete-all");
-deleteAll.addEventListener("click" , () => {
-    const ul = document.getElementsByClassName("collection");
-    ul[0].innerHTML = "";
+    //add fix task
+    const fix_button = document.createElement('button');
+    fix_button.innerHTML = 'Fix';
+    fix_button.setAttribute('class', 'btn fix');
+    li.appendChild(fix_button);
+    
+    fix_button.addEventListener('click', function edit(){
+        let old_value = p.innerText;
+        new_value = prompt(`Nhập giá trị muốn thay đổi cho ${old_value}`);
+        let htmls = `${new_value}`;
+        p.innerHTML = '';
+        p.innerHTML = htmls;
+    })
 })
+
+// addTask.addEventListener('click', () => newTask())
+
+//delete all task
+const delete_all = document.getElementById('delete-all');
+delete_all.addEventListener('click', () => {
+    const ul = document.getElementsByClassName('collection');
+    ul[0].innerHTML = '';
+})
+
