812f12762c86cc906af37afd405144f07b8ea4f4
letrung412 <64882738+letrung412@users.noreply.github.com>
2021-07-21 15:07:11 +0700

    update core

--- view.js
+++ view.js
@@ -2,8 +2,9 @@ export function ShowAllTask(items) {
     const ul = document.getElementsByClassName("collection");
     items.forEach(item => {
         const li = document.createElement("li");
-        li.innerHTML = item;
+        li.innerHTML = item.value;
         li.setAttribute("class" , "collection-item");
+        AddBtnDelete(li);
         ul[0].appendChild(li);
     })
 }
@@ -11,12 +12,29 @@ export function ShowAllTask(items) {
 export function AddTask(value) {
     const ul = document.getElementsByClassName("collection");
     const li = document.createElement("li");
-    li.innerHTML = value;
-    li.setAttribute("class" , "collection-item");
-    ul[0].appendChild(li);
+    if (value.length != 0) {
+        li.innerHTML = value;
+        li.setAttribute("class" , "collection-item");
+        AddBtnDelete(li);
+        ul[0].appendChild(li);
+        location.reload();
+    }
 }
 
 export function DeleteAll() {
     const ul = document.getElementsByClassName("collection");
     ul[0].innerHTML = ""
+}
+
+export function RemoveTask(li) {
+    li.remove();
+    location.reload();
+}
+
+function AddBtnDelete(li) {
+    const button = document.createElement("button");
+    button.innerHTML = "Delete";
+    li.appendChild(button);
+    button.setAttribute("class" , "btn delete");
+    button.addEventListener("click" , () => RemoveTask(li));
 }
